f00531db9fbb69bf24c169af7aa9ab138975cb09
Alexis Ortega <alexis.oc3@gmail.com>
2022-01-08 11:12:14 -0700

    Move creational patterns to directory

---
+++ creational/bridge.rb
@@ -0,0 +1,65 @@
+module Switch
+  attr_accessor :appliance
+
+  def turn_on
+    raise "Not implemented"
+  end
+
+  def turn_off
+    raise "Not implemented"
+  end
+end
+
+class RemoteControl
+  include Switch
+
+  def turn_on
+    appliance.turn_on
+  end
+
+  def turn_off
+    appliance.turn_off
+  end
+end
+
+module Appliance
+  def turn_on
+    raise "Not implemented"
+  end
+
+  def turn_off
+    raise "Not implemented"
+  end
+end
+
+class Lamp
+  include Appliance
+
+  def turn_on
+    puts "Turning the lamp on"
+  end
+
+  def turn_off
+    puts "Turning the lamp off"
+  end
+end
+
+class TV
+  include Appliance
+
+  def turn_on
+    puts "Turning the tv on"
+  end
+
+  def turn_off
+    puts "Turning the tv off"
+  end
+end
+
+remote_control = RemoteControl.new
+
+remote_control.appliance = Lamp.new
+remote_control.turn_on
+
+remote_control.appliance = TV.new
+remote_control.turn_on
